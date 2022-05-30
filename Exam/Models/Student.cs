38fc07cb1d75bcb3636420840116d117d8a4059b
bojan.andreevski <boki-k4@live.com>
2022-05-30 10:01:30 +0200

    add: Family tree diagram

--- Exam/Models/Student.cs
+++ Exam/Models/Student.cs
@@ -9,8 +9,7 @@
         public string Email { get; set; }
         public string Address { get; set; }
         public List<ExamsTaken> Exams { get; set; }
-        public int RecordCardId { get; set; }
-        public RecordCard RecordCard { get; set; }
+        public List<RecordCard> RecordCards { get; set; }
 
     }
 }
